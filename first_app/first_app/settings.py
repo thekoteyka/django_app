f5bdae7056ae2f842bba65d0c01e3ba48e6e532b
koteyka5000 <romacat5000@gmail.com>
2023-03-17 00:04:46 +0300

    static root

--- first_app/first_app/settings.py
+++ first_app/first_app/settings.py
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'staticbase' / 'media'
+STATIC_ROOT = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
